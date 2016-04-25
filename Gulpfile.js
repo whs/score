0b776589510738a22b17e8a404cc201790c9e054
Manatsawin Hanmongkolchai <manatsawin+git@gmail.com>
2016-04-25 11:20:54 +0700

    Centralized build config file

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,13 +6,16 @@ var cssnano = require('cssnano');
 var postcss_import = require('postcss-import');
 var postcss_url = require('postcss-url');
 var concat = require('gulp-concat');
+var preprocess = require('gulp-preprocess');
+var config = require('./config.json');
 
-gulp.task('default', ['copy-frontend', 'copy-backend', 'copy-scoreshare', 'build-frontend-js', 'build-frontend-css']);
+gulp.task('default', ['copy-frontend', 'copy-backend', 'copy-scoreshare', 'build-html', 'build-frontend-js', 'build-frontend-css']);
 
 gulp.task('copy-frontend', function(){
 	return gulp.src([
 		'frontend/.htaccess',
 		'frontend/**/*',
+		'!frontend/index.html',
 		'!frontend/js/**/*',
 		'!frontend/js',
 		'!frontend/css/**/*.css',
@@ -35,6 +38,12 @@ gulp.task('copy-scoreshare', function(){
 		.pipe(gulp.dest('dist/scoreshare/'));
 });
 
+gulp.task('build-html', function(){
+	return gulp.src('frontend/index.html', {base: 'frontend'})
+		.pipe(preprocess({context: config}))
+		.pipe(gulp.dest('dist/'));
+});
+
 gulp.task('build-asmcrypto', function(){
 	return gulp.src([
 		'node_modules/asmcrypto.js/src/errors.js',
@@ -50,25 +59,38 @@ gulp.task('build-asmcrypto', function(){
 });
 
 gulp.task('build-frontend-js', ['build-asmcrypto'], function(){
-	return gulp.src('frontend/js/app.js')
-		.pipe(webpack({
-			output: {
-				filename: 'app.js',
-			},
-			plugins: [
-				// new webpackModule.optimize.UglifyJsPlugin()
-			],
-			resolve: {
-				alias: {
-					asmcrypto: __dirname + '/dist/js/asmcrypto.js'
-				}
+	var webpackConfig = {
+		output: {
+			filename: 'app.js',
+		},
+		plugins: [
+			// new webpackModule.optimize.UglifyJsPlugin()
+		],
+		resolve: {
+			alias: {
+				asmcrypto: __dirname + '/dist/js/asmcrypto.js'
 			}
-		}))
+		}
+	};
+
+	if(config.useCdn){
+		webpackConfig.externals = {
+			jquery: 'jQuery',
+			'jquery-ui/dialog': 'jQuery',
+			'jquery-ui/button': 'jQuery',
+			'jquery-ui/effect-clip': 'jQuery',
+			'jquery-ui/mouse': 'jQuery',
+		};
+	}
+
+	return gulp.src('frontend/js/app.js')
+		.pipe(webpack(webpackConfig))
 		.pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('build-frontend-css', function(){
 	return gulp.src('frontend/css/style.css')
+		.pipe(preprocess({context: config}))
         .pipe(postcss([
 			postcss_import(),
 			postcss_url(),
