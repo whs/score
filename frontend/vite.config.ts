7b17119c90c9e02e26020bc639a966467d8afefa
Manatsawin Hanmongkolchai <git@whs.in.th>
2024-11-19 20:42:43 +0700

    Remove symfony

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,13 @@
 import type { UserConfig } from 'vite';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
 import license from 'rollup-plugin-license';
-import symfony from 'vite-plugin-symfony';
 import * as path from 'node:path';
 
 export default {
 	appType: 'mpa',
 	base: '',
 	build: {
-		rollupOptions: {
-			input: {
-				main: path.resolve(__dirname, 'index.html'),
-				adminCSS: './node_modules/@picocss/pico/css/pico.min.css',
-			},
-		},
 		outDir: 'dist',
-		manifest: true,
 		assetsInlineLimit: (filePath, _content) => {
 			// Force the eastereggs to use data URL
 			if (filePath.match(/easteregg\/(.*?)\.(webp|mp4)$/)) {
@@ -36,7 +28,6 @@ export default {
 		}),
 		// @ts-ignore https://github.com/asyncLiz/rollup-plugin-minify-html-literals/issues/24
 		minifyHTML.default(),
-		symfony(),
 	],
 	optimizeDeps: {
 		force: true,
